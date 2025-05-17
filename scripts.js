fe1108eb80ee4ce92b0c934c4f37cdb871e2ad36
arizu14n <arizu14n@gmail.com>
2025-05-17 14:29:02 -0300

    Programación 3

--- scripts.js
+++ scripts.js
@@ -1,64 +1,4 @@
-/* var top3 = document.getElementById("top3");
-var top15 = document.getElementById("top15");
-
-const url_pelis_populares = "https://api.themoviedb.org/3/movie/popular";
-const url_pelis_mejor_calificadas = "https://api.themoviedb.org/3/movie/top_rated";
-const url_pelis_detalles = "https://api.themoviedb.org/3/movie/";
-const lenguaje = "es-MX";
-const pagina = 1;
-const api_key = "bb7e28b3b7498216d239c131b96d75c2";
-
-const spinner = `
-                <div class="d-flex justify-content-center align-items-center" style="height: 400px">
-                    <div class="spinner-border" role="status">
-                        <span class="visually-hidden">Cargando...</span>
-                    </div>
-                </div>;`
-
-async function CargaPeliculasTop3(type) {
-    try {
-        let url;
-        let contenido_pelis_resp = "";
-        if( type === "top_rated") {
-            url = url_pelis_mejor_calificadas;
-        }
-        else if (type === "popular") {
-            url = url_pelis_populares;
-        }
-        top3.innerHTML = spinner;
-        const respuesta = await fetch(
-            `${url}?api_key=${api_key}&language=${lenguaje}&page=${pagina}`
-            );
-        if (respuesta.status === 200) {
-        const datos = await respuesta.json();
-        datos.results.forEach((pelicula) => {
-           contenido_pelis_resp += `<div class="carousel-item">
-            <img src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}" class="d-block w-75 m-auto rounded-pill" alt="...">
-            <div class="carousel-caption d-md-block" id=${pelicula.id}>
-              <h4 class="carru">Puesto 2</h4>
-              <p class="carru fs-3 fw-bold">${pelicula.title}</p>
-              <a class="btn btn-buscar" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">+Info</a>
-            </div>
-          </div>`;
-         
-        });
-        top3.innerHTML = contenido_pelis_resp;
-    } else if (respuesta.status === 401) {
-        console.log ("Error de logueo")
-    } else {
-        console.log(`Error al realizar la consulta. Código de error: ${respuesta.status}`);
-    }
-    }
-
-    catch (error) {
-        console.log(error)
-    }
-}
-
-CargaPeliculasTop3("top_rated"); */
-
-var top3 = document.getElementById("top3");
-var top15 = document.getElementById("top15");
+var top20 = document.getElementById("top20");
 let boton_en_cartelera = document.getElementById("button-cartelera");
 let boton_mejor_calificadas = document.getElementById("button-mejor-calificadas");
 let boton_populares = document.getElementById("button-populares");
@@ -125,23 +65,24 @@ if (boton_proximos_estrenos) {
    }
 
 
-async function CargaPeliculasTop(type) {
+async function CargaPeliculasTop(categoria) {
     try {
         let url;
         let contenido_pelis_resp = "";
-        if( type === "top_rated") {
-            url = url_pelis_mejor_calificadas;
-            }
-        else if (type === "popular") {
-            url = url_pelis_populares;
-            }
-        else if (type === "now_playing") {
-            url = url_pelis_en_cartelera;
-            }
-        else if (type === "upcoming") {
-            url = url_pelis_proximos_estrenos;
-            }
-        top15.innerHTML = spinner;
+            if( categoria === "top_rated") {
+                url = url_pelis_mejor_calificadas;
+                }
+            else if (categoria === "popular") {
+                url = url_pelis_populares;
+                }
+            else if (categoria === "now_playing") {
+                url = url_pelis_en_cartelera;
+                }
+            else if (categoria === "upcoming") {
+                url = url_pelis_proximos_estrenos;
+                }
+        top20.innerHTML = spinner;
+
         const respuesta = await fetch(
         `${url}?api_key=${api_key}&language=${lenguaje}&page=${pagina}`
         );
@@ -160,7 +101,7 @@ async function CargaPeliculasTop(type) {
             </div>`;
                          
                         });
-                        top15.innerHTML = contenido_pelis_resp;
+                        top20.innerHTML = contenido_pelis_resp;
                     } else if (respuesta.status === 401) {
                         console.log ("Error de logueo")
                     } else {
@@ -218,7 +159,7 @@ async function CargaPeliculasTop(type) {
                 };
 
                 
-document.addEventListener("show.bs.modal", async (event) => {
+    document.addEventListener("show.bs.modal", async (event) => {
     let id = event.relatedTarget.dataset.peliculaId;
     CargaPeliculasPorId(id);
-} )              ;
+});
